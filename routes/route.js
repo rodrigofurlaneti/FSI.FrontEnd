ee89b146e80d7721859d9d4b6a3f5955473294e7
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2025-01-09 10:03:12 -0300

    Add routes dymanic

---
+++ routes/route.js
@@ -0,0 +1,7 @@
+const urlBaseApi = "https://localhost:44337/api/";
+const urlBaseApiCategory = "https://localhost:44337/api/Categories";
+const urlBaseApiCommand = "https://localhost:44337/api/Commands";
+const urlBaseApiOrder = "https://localhost:44337/api/Orders";
+const urlBaseApiProduct = "https://localhost:44337/api/Products";
+const urlBaseApiProfile = "https://localhost:44337/api/Profiles";
+const urlBaseApiUser = "https://localhost:44337/api/Users";
